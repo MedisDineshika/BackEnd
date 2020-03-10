e3bdc1a331c6dd358c1ec1c5f9747cfdc888309f
Waruni Dineshika <dineshikamedis@gmail.com>
2020-03-10 06:25:33 +0530

    Initial Commit-AccountMgt

---
+++ accountMgtService/src/main/java/com/example/accountMgtService/AccountMgtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountMgtService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMgtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMgtServiceApplication.class, args);
+	}
+
+}
